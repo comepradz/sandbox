9da4c6e802d76b322f109f61ec3aae292bdf5767
comepradz <comepradz@gmail.com>
2013-07-31 08:38:08 +0700

    vector.py

---
+++ vector.py
@@ -0,0 +1,11 @@
+#Calculate sum of two vectors
+#
+x1 = input("x1 = ")
+y1 = input("y1 = ")
+x2 = input("x2 = ")
+y2 = input("x2 = ")
+
+xsum = x1+x2
+ysum = y1+y2
+
+print "(" + str(x1) + "i+" + str(y1) + "j) + (" + str(x2) + "i+" + str(y2) + "j) = " + str(xsum) + "i+" + str(ysum) + "j" 
